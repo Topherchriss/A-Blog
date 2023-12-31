4e13077780d90548012ae5af3aa95e78eb136030
Topherchriss <outlawsrabut@gmail.com>
2023-12-31 20:13:37 +0300

    Home page

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', post_detail, name='post_detail'),
     path('home/', blog_home, name='blog_home'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
 
 if settings.DEBUG:
