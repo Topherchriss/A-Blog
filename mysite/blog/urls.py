542b52061d245893eee3ff79669ecbf90f19a437
Topherchriss <outlawsrabut@gmail.com>
2023-12-31 18:08:30 +0300

    Included a base template file

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import post_list, post_detail
+from .views import post_list, post_detail, blog_home
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -10,7 +10,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', post_detail, name='post_detail'),
+    path('home/', blog_home, name='blog_home'),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
